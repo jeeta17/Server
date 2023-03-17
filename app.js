61bcd9ac99b919ea594579189fbc9d935cb5bbcf
jeeta17 <127959621+jeeta17@users.noreply.github.com>
2023-03-17 20:12:59 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -8,5 +8,5 @@ app.use(cors())
 app.listen(5000, () => console.log('connected to Ranjeet'))
 
 app.get('/', (req, res) => {
-        res.send({'name':'ranjeet'})
+    res.sendFile('./countries.json')
 })
